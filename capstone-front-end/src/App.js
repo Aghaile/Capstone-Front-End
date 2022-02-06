ddee1a736cfa7f2dbbd428627a9c115ee366882b
Ro Peeples <rogpeeples@gmail.com>
2022-02-06 11:55:17 -0700

    changed welcome page

--- capstone-front-end/src/App.js
+++ capstone-front-end/src/App.js
@@ -1,10 +1,7 @@
 import Welcome from './Pages/Welcome';
 import NewProfileForm from './Pages/NewProfileForm';
 import PetProfile from './Pages/PetProfile';
-import ErrorPage from './Pages/ErrorPage';
-import React, {useEffect, useState} from 'react';
-import BigSquare from './Components/BigSquare';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -15,7 +12,8 @@ function App() {
   <Router>
     <Routes>
       <Route path="/welcome"element={<Welcome/>}/>
-      <Route path="/profile"element={<PetProfile/>} />
+      <Route path="/profile/:login"element={<PetProfile/>} />
+      {/* <Route path="/pet"element={<PetProfile/>} /> */}
       <Route path="/"element={<NewProfileForm/>} />
       {/* <Route path="*"element={<ErrorPage/>} /> */}
     </Routes>
@@ -24,6 +22,64 @@ function App() {
 };
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import "./app.css";
+// import { Provider } from "react-redux";
+// import { applyMiddleware } from "redux";
+
+// import reducer from "./reducer";
+// import { createStore } from "redux";
+
+// import NavBar from "./components/Nav";
+// import { Typography, Divider } from "@material-ui/core";
+
+// import AuthRoute from "./Components/AuthRoute.js";
+
+// import HomePage from "./pages/HomePage";
+// import LoginPage from "./pages/Login";
+
+// import { appMiddleware } from "./middlewares/app";
+// import { apiMiddleware } from "./middlewares/core";
+// import MyAccount from "./pages/MyAccount";
+
+// const createStoreWithMiddleware = applyMiddleware(
+//   appMiddleware,
+//   apiMiddleware
+// )(createStore);
+
+// const store = createStoreWithMiddleware(reducer);
+
+// const IndexPage = () => (
+//   <>
+//     <Typography variant="h3">Welcome to the App</Typography>
+//     <Divider style={{ marginTop: 10, marginBottom: 10 }} />
+//     <Typography variant="h6">Feel free to take a look around</Typography>
+//   </>
+// );
+
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <Router>
+//         <NavBar />
+//         <div className="container">
+//           <Switch>
+//             <AuthRoute path="/home" render={HomePage} type="private" />
+//             <AuthRoute path="/login" type="guest">
+//               <LoginPage />
+//             </AuthRoute>
+//             <AuthRoute path="/my-account" type="private">
+//               <MyAccount />
+//             </AuthRoute>
+//             <Route path="/" render={IndexPage} />
+//           </Switch>
+//         </div>
+//       </Router>
+//     </Provider>
+//   );
+// }
 
 
 
--- capstone-front-end/src/Pages/Welcome.js
+++ capstone-front-end/src/Pages/Welcome.js
@@ -1,39 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
-import SearchBar from "../Components/SearchBar";
-
-// import { useParams } from "react-router-dom";
 
 const Welcome = () => {
-
-    const [petData, setPetData] = useState([]);
-    const [loggedInPet, setLoggedInPet] = useState([]);
-
-    const loginYourPet = (pet) => {
-        let petEndpoint =
-          process.env.REACT_APP_BACKEND_URL + "/profile/" + pet.id;
-    
-        axios
-          .get(petEndpoint)
-          .then((response) => {
-            setPetData([...response.data]);
-            setLoggedInPet(pet);
-          })
-          .catch((err) => console.log(err));
-      };
-    
-
+      
     return (
         <div>
-            <h1>Welcome</h1>
-                <div>
-                    <input type="text" placeholder="Enter your pet id!"/> 
-                </div>
-                <button onClick={loginYourPet}><Link to="/profile">Log In</Link></button>
-                <button><Link to="/">Create Profile</Link></button>
-            </div>
+        <input type="text" placeholder="Enter your pet id!"/> 
+        <button><Link to="/profile/:login">Log In</Link></button>
+        <button><Link to="/">Create Profile</Link></button>
+        </div>
     );
-};
+    };
+      
 
 export default Welcome;
